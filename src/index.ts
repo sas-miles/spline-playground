62118eac099cef0c823f369351e1b1b40e6542b1
sas-miles <132398659+sas-miles@users.noreply.github.com>
2023-11-15 13:18:02 -0500

    spline install

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import { greetUser } from '$utils/greet';
+import { Application } from '@splinetool/runtime';
 
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+const canvas = document.querySelector('.spline canvas');
+const spline = new Application(canvas);
+spline.load('https://prod.spline.design/u7doFV6D2jGUmb7U/scene.splinecode').then(() => {
+  //const obj = spline.findObjectByName('Cube');
+  // or
+  const obj = spline.findObjectById('aeb2e4df-4c91-442f-a2d7-cf01d5fa0144');
+
+  console.log(obj); // Spline Object => { name: 'Cube', id: '7AF5EBC0-09BB-4720-B045-F478F8053AA4', position: {}, ... }
+
+  // move the object in 3D space
+  obj.position.x += 10;
 });
